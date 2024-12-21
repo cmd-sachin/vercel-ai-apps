a1b4e4d56b2dcb1e8443341a2fb34370e6d71c17
Sachin <sachiniyyappan2005@gmail.com>
2024-12-21 10:14:05 +0530

    Delete BIT BOT directory

--- BIT BOT/src/app/api/chat/registry.js
+++
@@ -1,6 +0,0 @@
-import { createGoogleGenerativeAI } from "@ai-sdk/google";
-import { experimental_createProviderRegistry as createProviderRegistry } from "ai";
-
-export const registry = createProviderRegistry({
-  google: createGoogleGenerativeAI({ apiKey: process.env.API_KEY }),
-});
